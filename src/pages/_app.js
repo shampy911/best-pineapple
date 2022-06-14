6ddec9e45b49630c0121f2605cf21d292b6e3a6e
Stackbit Code Editor <projects@stackbit.com>
2022-06-14 07:26:58 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,6 +13,5 @@ import '../sass/main.scss';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
-    <script
-    <strategy=lazyunload
+
 }
