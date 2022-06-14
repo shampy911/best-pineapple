801674bd5f435c10e26114efa58baf7df2a755c7
Stackbit Code Editor <projects@stackbit.com>
2022-06-14 07:31:18 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,7 @@ import '../sass/main.scss';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
-<Script src=<script async src="https://www.googletagmanager.com/gtag/js?id=G-2JV2WQ4T0X"></script>
+<Script src=<script async src="https://www.googletagmanager.com/gtag/js?id=G-2JV2WQ4T0X" />
 
 
 }
