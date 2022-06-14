9817450a6286e95126f89b87286cc7ff8530b375
Stackbit Code Editor <projects@stackbit.com>
2022-06-14 07:27:04 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,6 @@
 import '../sass/main.scss';
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-2JV2WQ4T0X"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-2JV2WQ4T0X');
-</script>
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
